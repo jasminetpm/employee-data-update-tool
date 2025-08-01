bc932fedfdecfb5256fb05c6b522c8125ee9efe1
pk-wu <pattanapong1997k@gmail.com>
2025-08-01 15:57:02 +0800

    Added placeholder codes for Driver class structure

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println();
+
+        // Create empty Stack<Command> history to store history
+//        Stack<Command> history = new Stack();
+
+
+        // loop to take payload
+//        AddCommand addCommand = new AddCommand();
+//        UpdateCommand updateCommand = new UpdateCommand();
+//        DeleteCommand deleteCommand = new DeleteCommand();
+//        ListCommand listCommand = new ListCommand();
+//        UndoCommand undoCommand = new UndoCommand();
+//        Command[] commands = {
+//                addCommand,
+//                updateCommand,
+//                deleteCommand,
+//                listCommand,
+//                undoCommand
+//        };
+
+        // create invoker object
+//        Invoker invoker = new Invoker();
+
+        // set commands for invoker
+//        invoker.setCommandsForExecution(commands);
+
+        // execute the commands
+//        invoker.executeCommand(Stack<Command> history) {
+//            history.push(new Command());
+//        }
     }
 }
