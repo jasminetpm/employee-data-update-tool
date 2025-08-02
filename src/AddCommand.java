f8b66c83cf2f5c8f0a5af0e5fb9aa147eb3b3906
pk-wu <pattanapong1997k@gmail.com>
2025-08-03 03:51:27 +0800

    Added constructor and execute method for String payload

--- src/AddCommand.java
+++ src/AddCommand.java
@@ -1,14 +1,27 @@
 public class AddCommand implements Command {
     //params
     private Employee employee;
+    private String payload;
 
     //receiver
     private Receiver receiver;
+
+    // constructor for receiver with String payload params
+    public AddCommand(Receiver receiver, String payload) {
+        this.receiver = receiver;
+        this.payload = payload;
+    }
 
     public AddCommand(Receiver receiver, Employee employee) {
         this.receiver = receiver;
         this.employee = employee;
     }
+    // execute method for String payload
+//    @Override
+//    public void execute() {
+//        receiver.add(payload);
+//    }
+
 
     @Override
     public void execute() {
