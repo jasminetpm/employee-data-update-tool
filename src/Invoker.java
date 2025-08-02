b6fd2cc8470b33e591034df99f1e8f74be32c395
pk-wu <pattanapong1997k@gmail.com>
2025-08-03 00:10:56 +0800

    added storeToFile() method in Invoker

--- src/Invoker.java
+++ src/Invoker.java
@@ -1,3 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Stack;
 
 public class Invoker {
@@ -18,6 +23,28 @@ public class Invoker {
                 cmd.execute();
                 history.push(cmd);  // Only add to history if undoable
             }
+        }
+        // store successfully executed commands into the dataStore.txt
+        storeToFile(history);
+    }
+
+    /**
+     *  storeToFile() takes in filename and history stack, and overwrites the
+     *  dataStore.txt after every successful executeCommand
+     * @param history Command Stack history of successfully executed commands
+     */
+    public static void storeToFile(Stack<Command> history) {
+        Path filePath = Paths.get("src/dataStore.txt");
+
+        try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+            for (Command cmd : history) {
+                writer.write(cmd.toString());
+                writer.newLine();
+            }
+            System.out.println("Command history saved to " + filePath.toAbsolutePath());
+
+        } catch (IOException e) {
+            System.err.println("Error: Failed to write file: " + e.getMessage());
         }
     }
 }
