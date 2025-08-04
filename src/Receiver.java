da669e1af16d92d360e06c680462ce689393cf1c
pk-wu <pattanapong1997k@gmail.com>
2025-08-04 18:56:49 +0800

    updated receiver's update method

--- src/Receiver.java
+++ src/Receiver.java
@@ -119,51 +119,15 @@ public class Receiver {
      * @param params payload String in form of <index> <data>...
      * @return boolean true/false based on operation pass/fail
      */
-    public boolean update(String params, Employee updatedEmployee) {
-        String[] parts = params.split(" ");
-        if (parts.length < 2 || parts.length > 4) {
-            System.out.println("Invalid payload format. Expected: <index> <data1> <data2> <data3>");
-            return false;
-        }
 
-        int index;
-        // get the value of index from our String params
-        try {
-            index = Integer.parseInt(parts[0]) - 1; // Convert to 0-based index
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid index format.");
-            return false;
-        }
-
-        // check index exists within our employees list
-        if (index < 0 || index >= employees.size()) {
-            System.out.println("Index out of bounds.");
-            return false;
-        }
-        // retrieve the employee object stored at that index
-        Employee emp = employees.get(index);
-
-        // Update fields based on how many parts are present
-        // update only firstName
-        if (parts.length >= 2) {
-            emp.setFirstName(toTitleCase(parts[1]));
-        }
-        // update firstName + secondName
-        if (parts.length >= 3) {
-            emp.setLastName(toTitleCase(parts[2]));
-        }
-        // update all 3 data fields
-        if (parts.length == 4) {
-            String data3 = parts[3];
-            if (!isValidEmail(data3)) {
-                System.out.println("Invalid format for <data3>.");
-                return false;
-            }
-            emp.setEmail(data3);
-        }
-
-        System.out.println("Updated: " + emp);
-        return true;
+    /**
+     * update method taking index and employee object
+     * and updates (sets) the employee arraylist
+     * @param index index of employee to update
+     * @param updatedEmployee employee object we will use for overwriting
+     */
+    public void update(int index, Employee updatedEmployee) {
+        employees.set(index, updatedEmployee);
     }
 
 
