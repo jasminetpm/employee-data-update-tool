fc18ca9f80988c0a7dfd876189fb0186f45b66d2
pk-wu <pattanapong1997k@gmail.com>
2025-08-04 15:37:18 +0800

    added test codes

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,8 @@ import java.util.Stack;
 
 public class Client {
     public static void main(String[] args) {
+        // history
+        Stack<Command> history = new Stack<>();
 
         // Create Receiver
         Receiver receiver = new Receiver();
@@ -11,18 +13,23 @@ public class Client {
         Invoker invoker = new Invoker();
 
         // Test 1
-        Employee employee1 = new Employee("John",  "Doe", "simple@example.com");
+        Employee employee1 = new Employee("Jane",  "Doe", "simple@example.com");
         Command command1 = new AddCommand(receiver, employee1);
         Employee employee2 = new Employee("Hanna", "Moon", "tetter.tots@potatoesarelife.com");
         Command command2 = new AddCommand(receiver, employee2);
-        Command command3 = new DeleteCommand(receiver,2);
-        Command command4 = new ListCommand(receiver);
-        Command[] cmdToExecute = new Command[] {command1, command2, command3, command4};
+        Employee employee3 = new Employee("MAAA", "MooAAAn", "tetter.tots@potatoesarelife.com");
+        Command command3 = new AddCommand(receiver, employee3);
+
+        Command command4 = new DeleteCommand(receiver,2);
+//        Command command4 = new ListCommand(receiver);
+//        Command[] cmdToExecute = new Command[] {command1, command2, command3, command4};
 
         // Test 2
-//        Command command3 = new DeleteCommand(receiver,4);
-//        Command command4 = new ListCommand(receiver);
-//        Command[] cmdToExecute = new Command[] {command3, command4};
+//        Command command3 = new DeleteCommand(receiver,1);
+        Command listCommand = new ListCommand(receiver);
+        Command undoCommand = new UndoCommand(receiver,history);
+//        System.out.println(history);
+        Command[] cmdToExecute = new Command[] {command1, command2, command3, undoCommand, listCommand};
 
         // Test 3
 //        Command updateCommand = new UpdateCommand(receiver,"4 JANE DOE tots.tetter@gmail.com");
@@ -35,11 +42,11 @@ public class Client {
 
         // executing commands via invoker
         invoker.setCommandsForExecution(cmdToExecute);
-        Stack<Command> history = new Stack<>();
         invoker.executeCommand(history);
 
+
         // store updated employee ArrayList into the dataStore.txt at the end of program
-        receiver.storeToFile();
+//        receiver.storeToFile();
 
     }
 }
