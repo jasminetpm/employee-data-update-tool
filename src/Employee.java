5ffa98e64251bd27b9d13a03fa3e3979b2dddd95
pk-wu <pattanapong1997k@gmail.com>
2025-08-04 18:57:00 +0800

    added getters

--- src/Employee.java
+++ src/Employee.java
@@ -27,4 +27,16 @@ public class Employee {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
