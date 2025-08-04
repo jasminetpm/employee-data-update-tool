368122f452ecb9a7896a6b6f9977c718d2ae48b8
pk-wu <pattanapong1997k@gmail.com>
2025-08-04 15:39:11 +0800

    edit instance variables

--- src/UpdateCommand.java
+++ src/UpdateCommand.java
@@ -1,6 +1,7 @@
 public class UpdateCommand implements Command {
     //params
     private String params;
+    private Employee updatedEmployee;
 
     //receiver
     private Receiver receiver;
@@ -8,15 +9,20 @@ public class UpdateCommand implements Command {
     public UpdateCommand(Receiver receiver, String params) {
         this.receiver = receiver;
         this.params = params;
+        this.updatedEmployee = null; //TODO: fix this class
     }
 
     @Override
     public void execute() {
-        receiver.update(params);
+        receiver.update(params, updatedEmployee);
     }
 
     @Override
     public void undo() {
+//        receiver.getEmployees().add(deletedEmployee);
+    }
 
+    public void setUpdatedEmployee(Employee updatedEmployee) {
+        this.updatedEmployee = updatedEmployee;
     }
 }
