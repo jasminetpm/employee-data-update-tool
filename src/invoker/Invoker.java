f3d108a697f77f1224ee4fde3370c2298020d4d5
pk-wu <pattanapong1997k@gmail.com>
2025-08-06 15:19:27 +0800

    updated javadocs and comments

--- src/invoker/Invoker.java
+++ src/invoker/Invoker.java
@@ -6,30 +6,46 @@ import exceptions.CommandException;
 
 import java.util.Stack;
 
+/**
+ * Invoker class responsible for executing commands array through their
+ * respectively execute() methods.
+ * Invoker also pushes successful Add/Delete/Update Commands into history stack
+ */
 public class Invoker {
+    /**
+     * Array of Commands to be executed
+     */
     private Command[] cmdToExecute;
 
+    /**
+     * Sets command array for execution
+     * @param commands array of Command objects for execution
+     */
     public void setCommandsForExecution(Command[] commands) {
         this.cmdToExecute = commands;
     }
 
+    /**
+     * Executes commands in the cmdToExecute array.
+     * Each command calls execute and pushes to history for add/update/delete
+     * if they are successful, catching CommandExceptions if errors are encountered
+     * @param history Stack to track successfully executed commands
+     */
     public void executeCommand(Stack<Command> history) {
+        // if nothing to execute we can return
         if (cmdToExecute == null) {
             System.out.println("No commands set.");
             return;
-        }
-
-        else if (cmdToExecute.length == 0) {
+        } else if (cmdToExecute.length == 0) {
             System.out.println("No commands to undo.");
             return;
         }
-
+        // loop through the array of Commands and call their execute methods
         for (Command cmd : cmdToExecute) {
             try {
-                boolean undoableCommand = cmd.execute();
-                if (undoableCommand) { // Only add to history if undoable
-                    // add/update/delete returns true/false,
-                    // undo/list should always return false as they will never push to history
+                boolean doableCommand = cmd.execute();
+                if (doableCommand) {
+                    // push to history if doable add/update/delete
                     history.push(cmd);
                 }
             } catch (CommandException e) {
