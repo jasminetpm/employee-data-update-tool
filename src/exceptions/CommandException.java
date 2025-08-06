69e87a933141044b5c4627c1300fbd23e1f86800
pk-wu <pattanapong1997k@gmail.com>
2025-08-06 14:37:22 +0800

    updated javadocs and comments

--- src/exceptions/CommandException.java
+++ src/exceptions/CommandException.java
@@ -4,6 +4,10 @@ package exceptions;
  * Custom exception class for all non Throwable error messages
  */
 public class CommandException extends Exception {
+    /**
+     * Constructs a new CommandException with specific message.
+     * @param message details describing the error
+     */
     public CommandException(String message) {
         super(message);
     }
