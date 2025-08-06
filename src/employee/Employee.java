aa7dcc5d23f568e252e962ac622d4f578523bdcc
pk-wu <pattanapong1997k@gmail.com>
2025-08-06 14:21:47 +0800

    updated javadocs and comments

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -1,43 +1,67 @@
 package employee;
 
+/**
+ * The Employee class represents employees with fields:
+ * first name, last name, email address.
+ * This class is used throughout the command system to
+ * store and manipulate employee records.
+ */
 public class Employee {
+    /**
+     * employee's first name
+     */
     private String firstName;
+    /**
+     * employee's last name
+     */
     private String lastName;
+    /**
+     * employee's email address
+     */
     private String email;
 
+    /**
+     * Constructs new employee with specified params below
+     * @param firstName employee's first name
+     * @param lastName employee's last name
+     * @param email employee's email address
+     */
     public Employee(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
+    /**
+     * Returns string representation of employee in format:
+     * <firstName> <lastName> <email>
+     *
+     * @return formateed string representing the employee
+     */
     public String toString() {
         return firstName + " " + lastName + " " + email;
     }
 
-    // Getters and setters?
-
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    /**
+     * Returns employee's first name
+     * @return employee's first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Returns employee's last name
+     * @return employee's last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Returns employee's email or data3 equivalent
+     * @return employee's email or data3 equivalent
+     */
     public String getEmail() {
         return email;
     }
