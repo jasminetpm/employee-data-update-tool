d03f2e21d1437faa7e5d0c7ebfe7a0775246ba7c
pk-wu <pattanapong1997k@gmail.com>
2025-08-06 15:44:06 +0800

    updated javadocs and comments

--- src/commands/ListCommand.java
+++ src/commands/ListCommand.java
@@ -2,16 +2,32 @@ package commands;
 
 import receiver.Receiver;
 
+/**
+ * ListCommand class implements Command interface.
+ * This command displays the current list of employees
+ * by calling receiver.list();
+ */
 public class ListCommand implements Command {
-    // No params because no inputs required
 
-    //receiver
+    /**
+     * receiver object passed into the constructor from the client
+     */
     private Receiver receiver;
 
+    /**
+     * public constructor for the class ListCommand
+     *
+     * @param receiver object to link to receiver class
+     */
     public ListCommand(Receiver receiver) {
         this.receiver = receiver;
     }
 
+    /**
+     * executes list command by invoking receiver.list();
+     *
+     * @return boolean false so command is not pushed into history stack
+     */
     @Override
     public boolean execute() {
         System.out.println("List");
@@ -19,8 +35,11 @@ public class ListCommand implements Command {
         return false;
     }
 
+    /**
+     * empty body as list is read-only method that cannot be undone
+     */
     @Override
     public void undo() {
-
+        // no undo behaviour for read-only command
     }
 }
